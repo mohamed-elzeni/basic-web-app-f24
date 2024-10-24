b6ec69b3e7d7be7d7ede2b1967b29b5416f21655
Mohamed Elzeni <msmohame@andrew.cmu.edu>
2024-10-24 16:30:31 +0300

    real name

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -12,5 +12,11 @@ export default function QueryProcessor(query: string): string {
     //TODO update the corresponding test case in __tests__
     return ( "msmohame" );
   }
+
+  if (query.toLowerCase().includes("name")) {
+    //TODO add your Andrew ID below
+    //TODO update the corresponding test case in __tests__
+    return ( "msmohame" );
+  }
   return "";
 }
